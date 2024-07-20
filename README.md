edddbb3959a8aceb6daf5224dde6e36781e76397
LuceroVS <lu.vis.san19@gmail.com>
2024-07-19 18:55:11 -0600

    Edicion de variables

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Tripleten web_project_around
 
-# https://lucerovs.github.io/web_project_around/
+## Lucero Villafuerte
+
+Estudiante de software en Tripleten
+
+## Tecnologias
+
+[![html](https://developer.mozilla.org/es/docs/Web/HTML)]
+
+[![Javascript](https://developer.mozilla.org/es/docs/Web/JavaScript)]
+
+## Descripción
+
+Se muestra un perfil con el botón de editar, el cual despliega un popup permitiendo modificar los campos nombre y profesión.
+
+## Pagina
+
+```bash
+https://lucerovs.github.io/web_project_around/
+```
--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,39 @@
-profileEditButton = document.getElementById("profile_edit_button");
+let profileEditButton = document.getElementById("profile_edit_button");
 profileEditButton.addEventListener("click", showPopupContainer);
 
 function showPopupContainer() {
-  popupContainerForm = document.getElementById("popupContainer");
+  let popupContainerForm = document.getElementById("popupContainer");
   popupContainerForm.style.display = "block";
-  popupForm = document.getElementById("popup");
+  let popupForm = document.getElementById("popup");
   popupForm.style.display = "block";
 }
 
-profileClosePopupContainer = document.getElementById("popupContainerclose");
+let profileClosePopupContainer = document.getElementById("popupContainerclose");
 profileClosePopupContainer.addEventListener("click", closePopupContainer);
 
 function closePopupContainer() {
-  popupContainerForm = document.getElementById("popupContainer");
+  let popupContainerForm = document.getElementById("popupContainer");
   popupContainerForm.style.display = "none";
-  popupForm = document.getElementById("popup");
+  let popupForm = document.getElementById("popup");
   popupForm.style.display = "none";
 }
 
-profileSavePopupContainer = document.getElementById("save");
+let profileSavePopupContainer = document.getElementById("save");
 profileSavePopupContainer.addEventListener("click", saveModify);
 
 function saveModify() {
-  profileNameModify = document.getElementById("popupContainerName");
-  profileNameModifyValue = profileNameModify.value;
-  profileProfessionModify = document.getElementById("popupContainerProfession");
-  profileProfessionModifyValue = profileProfessionModify.value;
-  popupContainerForm = document.getElementById("popupContainer");
+  let profileNameModify = document.getElementById("popupContainerName");
+  let profileNameModifyValue = profileNameModify.value;
+  let profileProfessionModify = document.getElementById(
+    "popupContainerProfession"
+  );
+  let profileProfessionModifyValue = profileProfessionModify.value;
+  let popupContainerForm = document.getElementById("popupContainer");
   popupContainerForm.style.display = "none";
-  profileInfoName = document.getElementById("profileInfoName");
-  profileInfoJob = document.getElementById("profileInfoJob");
+  let profileInfoName = document.getElementById("profileInfoName");
+  let profileInfoJob = document.getElementById("profileInfoJob");
   profileInfoName.innerHTML = profileNameModifyValue;
   profileInfoJob.innerHTML = profileProfessionModifyValue;
-  popupForm = document.getElementById("popup");
+  let popupForm = document.getElementById("popup");
   popupForm.style.display = "none";
 }
